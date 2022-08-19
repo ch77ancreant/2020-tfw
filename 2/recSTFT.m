07f2a4791311c45e66cf2199b5fa75e9c24ee948
chc <99154196+ch77ancreant@users.noreply.github.com>
2022-08-19 12:07:30 +0800

    update files

--- 2/recSTFT.m
+++ 2/recSTFT.m
@@ -2,19 +2,20 @@ function y = recSTFT(x, t, f, B)
 
 dt = t(2) - t(1);
 df = f(2) - f(1);
-m = f/df;
 N = round(1/(dt*df));
+
+m = f/df;
 m1 = round(mod(m,N) + 1);
 Q = round(B/dt);
 
-for ii = 1:length(t)
+for n = 1:length(t)
     
     % generate x1(q)
     x1 = zeros(1, N);
     for q = 1:2*Q+1
-        a = ii - Q + q;
+        a = n - Q + q;
         if a>=1 && a<=length(t)
-        x1(1, q) = x(a);
+            x1(1, q) = x(a);
         end
     end
     
@@ -22,10 +23,8 @@ for ii = 1:length(t)
     X1 = fft(x1);
     
     % output STFT
-    y(:, ii) = X1(m1) .* dt * exp((1j*2*pi*(Q-ii)*m)/N);
+    y(:, n) = X1(m1) .* dt .* exp((1j*2*pi*(Q-n)*m)/N);
 end
 
 end
 
-
-    
